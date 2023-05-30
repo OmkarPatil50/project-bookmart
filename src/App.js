309d8f50e5e8a8897154faa576f5889d7e7fd06d
Omkar Patil <Omkarp5429@gmail.com>
2023-05-30 07:38:23 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import "./App.css";
+
+
+function App() {
+
+
+const getData = async()=>{
+
+}
+
+
+useEffect(()=>{getData()} , [])
+
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
